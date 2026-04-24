b0c985d936d2ceb2c68415b592b24a0318582fd2
Andrés Ricaño <aricanomx@gmail.com>
2026-04-24 12:03:21 -0600

    fix(mobile): resolve web bundling failure by reinstalling worklets and naming babel plugins

--- finwealth-mobile/babel.config.js
+++ finwealth-mobile/babel.config.js
@@ -2,6 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      ['react-native-worklets/plugin', {}, 'worklets-unique'],
+      ['react-native-reanimated/plugin', {}, 'reanimated-unique'],
+    ],
   };
 };
